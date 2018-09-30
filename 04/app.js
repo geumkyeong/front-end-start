033d7b1c3466ee7b45dd8baf73d749f9fa798e24
geumkyeong <geumkyeong@github.com>
2018-10-01 00:55:03 +0900

    edit 03

--- 04/app.js
+++ 04/app.js
@@ -2,14 +2,13 @@ var wrap = document.querySelector('.wrap');
 
 var str = '';
 
-//var 변수 재선언, 재할당 O, let 변수 재선언 X 재할당 O, const 변수 재선언, 재할당 X
-
 for (let i = 1; i <= 4; i++) {
     for (let j = 1; j <= 4; j++) {
-        let block = document.createElement('div');
-        block.className = (i + j) % 2 == 1 ? 'b' : 'w';
-        wrap.appendChild(block);
+        // let block = document.createElement('div');
+        // block.className = (i + j) % 2 == 1 ? 'b' : 'w';
+        // wrap.appendChild(block);
+        str += `<div class='${(i + j) % 2 == 1 ? 'b' : 'w'}'></div>`;
     }
 }
 
-// wrap.innerHTML = str;
+wrap.innerHTML = str;
