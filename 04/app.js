42120aeb87a437217a9dbac28072cf2d2464283a
geumkyeong <geumkyeong@github.com>
2018-10-01 00:14:54 +0900

    edit 01

--- 04/app.js
+++ 04/app.js
@@ -1,68 +1,22 @@
 var wrap = document.querySelector('.wrap');
+
 var str = '';
-
-for(let i = 1; i <= 4; i++) {
-  for(let j = 1; j <= 4; j++) {
-      // let block = document.createElement('div');
-      // block.className = (i + j) % 2 == 1 ? 'black' : 'white';
-      // wrap.appendChild(block);
-      str += `<div class='${(i + j) % 2 == 1 ? 'b' : 'w'}'></div>`  
-  }
-}
-
-
-wrap.innerHTML = str;
-
-var blocks = document.querySelectorAll('.wrap > div');
-
-// blocks.forEach(function(block){
-
-//   block.addEventListener('click', function(event) { 
-//     console.log(block);
-
-//     block.style.backgroundColor = 'yellow';
-//     // block.className += ' y';
-//   })
-
-// });
-
-//------------------------------------------------------------------
-
-// for(var i=0; i < 16; i++) {
-//   // console.log( blocks[i] );
-
-//   blocks[i].addEventListener('click', function(event) {
-
-//     debugger
-//     blocks[i].style.backgroundColor = 'yellow';
-    
-//   });
-
-// }
-
-//------------------------------------------------------------------
-var myVar = true;
-function select(event) {
-    var block = event.currentTarget; //선택된 이벤트 객체를 불러온다.
-
-    var myHandler = function(event) {
-        if(myVar) {
-            block.style.backgroundColor = 'yellow';
+for (var i = 0; i < 4; i++) {
+    for (var j = 0; j < 4; j++) {
+        if(i%2==0){
+            if(j%2==0){
+                str += '<div class="b"></div>';
+            } else {
+                str += '<div class="w"></div>';
+            }
         } else {
-            block.style.backgroundColor = 'red';
+            if(j%2==1){
+                str += '<div class="b"></div>';
+            } else {
+                str += '<div class="w"></div>';
+            }
         }
     }
+}
 
-    if(myVar) {
-        window.addEventListener('mousemove', myHandler, false);
-        myVar = false;
-    } else {
-        window.removeEventListener('mousemove', myHandler, false);
-        myVar = true;
-    }
-}
-for(var i=0; i < 16; i++) {
-    // console.log( blocks[i] );
-    blocks[i].addEventListener('click', select);
-  
-}
+wrap.innerHTML = str;
