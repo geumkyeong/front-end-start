7f47fd7dae3bb804cabd0531a603ec0e2bffe4bd
geumkyeong <geumkyeong@github.com>
2018-10-01 22:33:24 +0900

    edit 04

--- 05/app.js
+++ 05/app.js
@@ -1,17 +1,20 @@
 console.log('app.js');
 
 function json(){
-    fetch('./data.json')
+    var url = 'https://1boon.kakao.com/ch/enter.json?page=1&pagesize=1';
+    fetch(url)
         .then(function(response){
             response.json().then(function(data){
                 console.log('json data: ', data);
-                document.getElementById('name').innerHTML = data.name;
+                // document.getElementById('name').innerHTML = data.name;
             });
         })
         .catch(function(err){
             console.log('Fetch Error :-S', err);
         });
-}
+} 
+
+json();
 
 function jsonp(){
     var head = document.querySelector('head');
@@ -23,6 +26,6 @@ function jsonp(){
 function callback(data) {
     console.log(data)
     document.getElementById('name').innerHTML = data.name;
-}
+} //jacascript로 json 추가하기
 
-jsonp();
+// jsonp();
