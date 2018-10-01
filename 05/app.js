355a9af126bd4012d86813cd390aedae4f473506
geumkyeong <geumkyeong@github.com>
2018-10-01 22:06:10 +0900

    edit 01

--- 05/app.js
+++ 05/app.js
@@ -1,29 +1,9 @@
 console.log('app.js');
-var url = 'https://1boon.kakao.com/ch/enter.json?page=3&pagesize=2';
-function json(){
-        fetch(url)
-    .then(function(response){
+
+fetch('./data.json')
+.then(function(response){
     response.json().then(function(data){
-        console.log('json data:', data);
-        // document.getElementById('name').innerHTML = data.name;
+        console.log('json data: ', data);
+        //json data:  {name: "MGG", onsen: "tamago"}
     });
-    })
-    .catch(function(err) {
-        console.log('Fetch Error :-S', err);
-    });
-}
-
-function jsonp() {
-    var head = document.querySelector('head');
-    var script = document.createElement('script');
-    script.src = 'json.js';
-    head.appendChild(script);
-}
-
-function callback(data){
-    console.log(data);
-    document.getElementById('name').innerHTML = data.name;
-}
-
-json();
-// jsonp();
+});
