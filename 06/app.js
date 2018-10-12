10ca6de472132ad9949dd47f7ae2a0871f415d23
geumkyeong <geumkyeong@github.com>
2018-10-12 13:04:12 +0900

    edit 01

--- 06/app.js
+++ 06/app.js
@@ -1,20 +1,44 @@
 console.log('app');
 var appkey = 'b5f22ee8a7db9f54f69151f66c92861d';
-var query = '제주대학교';
-var url = `https://dapi.kakao.com/v2/search/web?query=${query}}`;
+var btn = document.getElementById("btn");
+var search = document.getElementById("search");
 
 
 var myHeaders = new Headers(); //헤더라는 객체로 헤더를 만듬
 myHeaders.append("Authorization", "KakaoAK b5f22ee8a7db9f54f69151f66c92861d");
-var option = {
+var options = {
     headers : myHeaders
-};
-//header 넘김
+};//header 넘김
 
+function searching(keyword) {
+    var url = `https://dapi.kakao.com/v2/search/web?query=${keyword}`;
+    getUrlData(url, options, board);
+}
 
-fetch(url, option)
-  .then(function(response){
-    response.json().then(function(data){
-        console.log('json data:', data);
+function board (json){
+    console.log(json);
+
+    var str = '';
+    for(var i = 0; i < json.data.length; i++){
+        var title = json.data[i].title;
+        var path = json.data[i].path;
+        str += '<a href="'+ path +'">' + title + '</a><BR>';
+    }
+    wrap.innerHTML = str;
+}
+
+function getUrlData(url, options, callback){
+    fetch(url, options).then(function(response){
+        response.json().then(function(data){
+            callback(data)
+        });
+    })
+    .catch(function(err){
+        console.log('Fetch Error :-S', err);
     });
+}
+
+btn.addEventListener('click', function(event){
+    var keyword = search.value;
+    searching(keyword);
 });
