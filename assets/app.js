6004037ba559d5d8c1bc174f49d7788a6859a1d0
geumkyeong <geumkyeong@github.com>
2018-10-21 14:57:06 +0900

    assets 02

--- assets/app.js
+++ assets/app.js
@@ -3,23 +3,38 @@ var page = 1;
 var pageCount = 20;
 var url = `https://1boon.kakao.com/ch/${channel}.json?pagesize=${pageCount}&page=${page}`;
 
+var str = '';
+var btn = document.querySelector('.btn');
 
-function setUrl(aTag) {
-    channel = document.getElementById(aTag).id;
+function btnClick() {
+    page++;
+    setUrl();
+    getUrlData(url, board);
+}
+
+function setUrl() {
     url = `https://1boon.kakao.com/ch/${channel}.json?pagesize=${pageCount}&page=${page}`;
 }
+
+btn.addEventListener('click', btnClick);
 
 //List
 var list = document.getElementById('list');
 
 function board(json) {
-    var str = '';
-    
     for(var i = 0; i < json.data.length; i++){
+        var coverImage = json.data[i].coverImage;
         var title = json.data[i].title;
         var path = json.data[i].path;
+        var totalView = json.data[i].totalView;
 
-        str += `<a href="https://1boon.kakao.com/${path}">${title}</a><BR>`;
+        str += `<a href="https://1boon.kakao.com/${path}" class="conList">
+            <span class="containImg">
+                <img src="${coverImage}" class="conImg" alt="">
+            </span>
+            <strong class="conTitle">${title}</strong>
+            <span class="conTotal">${totalView} 읽음</span>
+        </a><BR>`;
     }
 
     list.innerHTML = str;
@@ -40,8 +55,8 @@ function getUrlData(url, callback) {
     });
 }
 
-// Nav-bar action
-var clicked = document.querySelector('.active');//이미 클릭된 요소
+// Nav-bar
+var clicked = document.querySelector('.active'); //클릭된 요소
 var lies = document.querySelectorAll('.nav > li');
 
 lies.forEach(function(li){ // 각 요소에 이벤트 리스너 추가
@@ -50,12 +65,36 @@ lies.forEach(function(li){ // 각 요소에 이벤트 리스너 추가
 });
 
 function onClick() {
+    str = ''; page = 1; //list 초기화
+    channel = document.getElementById(event.target.id).id; //a-tag의 id가져오기
+
     clicked = document.querySelector('.active');
     clicked.className = clicked.className.replace(/\bactive\b/g, '');
     
     var click = event.currentTarget; //이벤트가 바인딩된 요소
     click.className = 'active';
 
-    setUrl(event.target.id);
+    setUrl();
     getUrlData(url, board);
 }
+
+//Scroll-bar
+
+// var $window = $(window);
+// var $glyphicon = $('.glyphicon')
+
+// function checkScroll(event) {
+//   var scrollTop = $window.scrollTop();
+//   var 업버튼이_보여질_포지션 = 100;
+
+//   if(scrollTop > 업버튼이_보여질_포지션) {
+//     $glyphicon.fadeIn();
+//   }
+// }
+// function goTop(){
+//   // $window.scrollTop(0); 
+//   $( 'html, body' ).animate( { scrollTop : 0 }, 400 )
+// }
+
+// $window.on('scroll', checkScroll);
+// $glyphicon.click(goTop);
