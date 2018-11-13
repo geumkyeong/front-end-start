5e17663b79f9f5bc063c2925b8517b044d498df6
geumkyeong <geumkyeong@github.com>
2018-11-13 15:49:39 +0900

    edit 01

--- 10/app.js
+++ 10/app.js
@@ -1,70 +1,77 @@
-var bug = document.querySelector('#bug');
-var box = document.querySelector('.box');
+var repeat = setInterval(move, speed);
 
-var min = 0;
-var max = 395;
+var $bug = document.querySelector('#bug');
+var $box = document.querySelector('.box');
+var $score = document.querySelector('#score');
+var $life= document.querySelector('#life');
+
+var stopInterval = false;
+
+var score = 0;
+var life = 10;
+var level = 1;
+var speed = 1500;
+var timer = 0;
 
 function getRandomArbitrary(min, max) {
-    return Math.floor(Math.random() * (max - min))+ min;
+    return Math.floor( Math.random() * (max - min) ) + min;
 }
 
 function move() {
-    var x = getRandomArbitrary(0, 395);
-    var y = getRandomArbitrary(0, 395);
-    console.log(x+''+y);
-    bug.style.left = x + 'px';
-    bug.style.top = y + 'px';
+    if(!stopInterval){
+        $bug.style.display = 'block';
+
+        var x = getRandomArbitrary(30, 300);
+        var y = getRandomArbitrary(30, 300);
+
+        console.log(x, y);
+        console.log(speed);
+
+        $bug.style.left = x + 'px';
+        $bug.style.top = y + 'px';
+    }
 }
 
-// function move() {
-//     bug.style.transform = translate(x, y);
-// }
-var timer = 0;
-var speed = 1000;
-
-var life = 10;
-var score = 0;
-var miss = 0;
-var level = 1;
-
-// function onClick (event) {
-//     console.log('click' + event);
-//     life--; score++; miss++;
-//     if(life == 0) { event.preventDefault(); };
-//     if(score >= 10) { level++; };
-//     if(miss == 10) { event.preventDefault(); };
-//     str = `Score: ${score} Level: ${level} Miss: ${miss}`;
-//     score.innerHTML = str;
-// }
-function start() {
-    clearInterval(timer);
-    setInterval(move, speed);
-}
-
-function addPoint(){
+function addScore(){
     score++;
-    score.innerHTML = score;
+    $score.innerHTML = score;
     dieBug(); //클릭했을 때 요소 없어짐
     //이벤트 전파방지 (클릭했을 때 자식요소도 이벤트 전파)
     event.stopPropagation();
 }
-
-function life() {
+  
+function discountLife() {
     life--;
-    life.innerHTML = life;
-    if(life === 0) { gameover(); };
+    $life.innerHTML = life;
+    if(life === 0) { gameover(); }
+}
+
+function setSpeed(){
+    if(score != 0 && score % 5 === 0) {
+        level++;
+        speed -= 500;
+    }
+}
+
+function start() {
+    setSpeed();
+    clearInterval(repeat);
+    repeat = setInterval(move, speed);
 }
 
 function gameover() {
-    alert('timeover');
-    clearInterval(timer);
+    clearInterval(repeat);
+    alert('GameOver');
+    stopInterval = true;
 }
+
 function dieBug() {
-    bug.style.display ='none';
+    $bug.style.display ='none';
+    clearInterval(repeat);
+    start();
 }
-
-bug.addEventListener('click', addPoint);
-
-
 
 start();
+
+$bug.addEventListener('click', addScore);
+$box.addEventListener('click', discountLife);
